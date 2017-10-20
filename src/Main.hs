4a2a19b0866ed2fe2b71d0547f31a91dffd59b7c
John C. Lavelle <jlavelle604@gmail.com>
2017-10-20 01:05:40 -0400

    Rudimentary effects system

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,8 @@
 module Main where
 
 import           Control.Monad             ((>=>))
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
 import qualified Data.ByteString.Lazy      as BS
 import           Data.Maybe                (catMaybes)
 import           Data.Monoid               ((<>))
@@ -17,7 +19,7 @@ import           Network.Wai.Handler.Warp  (run)
 
 data HttpContext' = HttpContext' { req :: Request, res :: Maybe Response }
 
-type HttpContext = Maybe HttpContext'
+type HttpContext = MaybeT IO HttpContext'
 
 data Config = Config { cfgPort :: Int }
 
@@ -36,28 +38,27 @@ pathMatches :: T.Text -> HttpContext' -> Bool
 pathMatches path ctx = pathInfo (req ctx) == testedPath
   where testedPath = filter (/= "") (T.splitOn "/" path)
 
+-- Picks the first match from the list of routes
 choose :: [HttpContext' -> HttpContext] -> HttpContext' -> HttpContext
-choose rs ctx = do
-  let matches = fmap ($ ctx) rs
-  case catMaybes matches of
-    []      -> Nothing
-    [match] -> Just match
-    _       -> error "Multiple route matches found!"
+choose [] _ = MaybeT $ return Nothing
+choose (r:rs) ctx = do
+  maybeMatch <- liftIO $ runMaybeT $ r ctx
+  maybe (choose rs ctx) (return . id) maybeMatch
 
 mustSatisfy :: (a -> Bool) -> a -> Maybe a
 mustSatisfy p a = if p a then Just a else Nothing
 
 get :: HttpContext' -> HttpContext
-get = mustSatisfy isGet
+get = MaybeT . return . mustSatisfy isGet
 
 post :: HttpContext' -> HttpContext
-post = mustSatisfy isPost
+post = MaybeT . return . mustSatisfy isPost
 
 path :: T.Text -> HttpContext' -> HttpContext
-path s = mustSatisfy (pathMatches s)
+path s = MaybeT . return . mustSatisfy (pathMatches s)
 
 ok :: BS.ByteString -> HttpContext' -> HttpContext
-ok r ctx = Just (ctx { res = Just $ textResponse r})
+ok r ctx = MaybeT $ return $ Just (ctx { res = Just $ textResponse r})
 
 textResponse :: BS.ByteString -> Response
 textResponse t = responseLBS status200 [(hContentType, "text/plain")] t
@@ -67,6 +68,8 @@ testApp = choose
   [ get >=> choose
     [ path "/" >=> ok "Homepage GET!"
     , path "/hello" >=> ok "Hello GET"
+    -- this route is simply ignored: 
+    , path "/hello" >=> ok "Hello from duplicate GET"
     , path "/goodbye" >=> ok "Goodbye GET" ]
   , post >=> choose
     [ path "/hello" >=> ok "Hello POST"
@@ -76,15 +79,17 @@ testApp = choose
 startWebServer :: Config -> WebPart -> IO ()
 startWebServer cfg wp = do
   let port = cfgPort cfg
-  run port (toWai wp)
+  app <- toWai wp
+  run port app
 
-toWai :: WebPart -> Application
-toWai wp r f = case wp (initialContext r) of
-  Just ctx -> case res ctx of
-    Just response -> f response
-    Nothing -> f $ responseLBS status500 [(hContentType, "text/plain")] "Internal Error."
-  Nothing -> f $ responseLBS status404 [(hContentType, "text/plain")] "Not Found!"
-
+toWai :: WebPart -> IO (Application)
+toWai wp = return $ \r f -> do
+  result <- runMaybeT $ wp (initialContext r)
+  case result of
+    Just ctx -> case res ctx of
+      Just response -> f response
+      Nothing -> f $ responseLBS status500 [(hContentType, "text/plain")] "Internal Error."
+    Nothing -> f $ responseLBS status404 [(hContentType, "text/plain")] "Not Found!"
 
 main :: IO ()
 main = startWebServer (Config 3000) testApp
